aa254d7e7e65364691242b7a1c6126ce31c48e5c
Syed Talha <syedt.in00@gmail.com>
2024-03-18 21:01:50 +0500

    modified folder stracture

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,21 +2,12 @@ import React from 'react'
 import "../App.css"
 
 
-const show = () => {
-    const currentLocation = document.getElementsByClassName("currentLocation");
-    for (let i = 0; i < currentLocation.length; i++) {
-        currentLocation[i].style.display = "block";
-    }
-}
-
 function Search() {
   return (
     <>
         <div className="searchBox">
             <span>
-            <img src={search_icon} alt="Search icon" className="searchIcon"/>
-            <input type="text" className="searchInput" placeholder="Search your City" onClick={show} />
-            <p className="print"></p>
+            <input type="text" className="searchInput" placeholder="Search your City" />
             </span>
         </div>
     </>
