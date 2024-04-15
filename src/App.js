8d6dad6eb708d8aac37ba1bcf4c395e7a45cb4b0
Talha Shah <syedt.in00@gmail.com>
2024-04-15 20:51:53 +0500

    Removed Forecast.jsx and modified Header.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Header from './components/Header';
-import WeatherApp from "./components/WeatherApp"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <WeatherApp />
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import Search from './Search'
-
-
+import "../App.css"
 
 
 function Header() {
   return (
     <div className="header">
-      <h1>Weather</h1>
+      <h1 className="logo">Weather App</h1>
+      <span className="credit">
+        <a href="https://github.com/talhashah-dev/weather-app-react" target="_blank" rel="noreferrer">Github</a>
+        <a href="https://www.linkedin.com/in/talhashah-dev/" target="_blank" rel="noreferrer">Linkedin</a>  
+      </span>      
     </div>
   )
 }
