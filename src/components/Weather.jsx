e4a9b4bb7257edfcce8899cb97c7c09adfbd7c84
Talha Shah <syedt.in00@gmail.com>
2024-04-17 00:19:42 +0500

    UI modified and new icons added

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
+import Arrow_Up_Icon from "../weather_icons/arrow-up.png";
+import Arrow_Down_Icon from "../weather_icons/arrow-down.png";
+import Humidity_Icon from "../weather_icons/humidity.png";
+import Wind_Icon from "../weather_icons/wind_icon.png";
+import Pressure_Icon from "../weather_icons/pressure.png";
+import Clear from "../weather_icons/clear.png"
+import Wind from "../weather_icons/wind.png"
+import Cloudy from "../weather_icons/cloudy.png"
+import Lightning from "../weather_icons/lightning.png"
+import Haze from "../weather_icons/haze.png"
+import Rain from "../weather_icons/rain.png"
+import Snow from "../weather_icons/snow.png"
 
 function Weather() {
-  const API_KEY = "210eeabe1cac851c368047662c4815fd";
+  // const API_KEY = "210eeabe1cac851c368047662c4815fd";
   const [inpValue, setInpValue] = useState("");
-  const [weatherData, setWeatherData] = useState("");
+  // const [weatherData, setWeatherData] = useState("");
 
   // Function for fetching data from API on key press
   const handleKey = async (event) => {
@@ -11,13 +23,10 @@ function Weather() {
       if (inpValue !== "") {
         console.log("working...", inpValue);
         try {
-          let url = `https://api.openweathermap.org/data/2.5/weather?q=${inpValue.replace(
-            /\s/g,
-            ""
-          )}&units=metric&appid=${API_KEY}`;
-          let response = await fetch(url);
-          let data = await response.json();
-          setWeatherData(data);
+          // let url = `https://api.openweathermap.org/data/2.5/weather?q=${inpValue.replace(/\s/g,"")}&units=metric&appid=${API_KEY}`;
+          // let response = await fetch(url);
+          // let data = await response.json();
+          // setWeatherData(data);
         } catch (error) {
           console.log("error", error);
         }
@@ -52,30 +61,42 @@ function Weather() {
               <img
                 className="icon"
                 width={"100px"}
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJkPC6jNO4CP_14rB_W8VmlfpvF0uCuG2fU9z7geagHg&s"
+                src={Rain}
                 alt="Weather Icon"
               />
-              <p className="temp">26</p>
+              <p className="temp">26°</p>
             </div>
             <p className="description">Clear</p>
           </div>
 
           <div className="extraInfo">
-            <p className="feelsLike">Feels like 26</p>
+            <p className="feelsLike">Feels like 26°</p>
             <div className="tempContainer">
               <div className="tempBox">
-                <img width={"20px"} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJkPC6jNO4CP_14rB_W8VmlfpvF0uCuG2fU9z7geagHg&s" alt="" />
-                <p className="max_temp">26</p>
+                <img src={Arrow_Up_Icon} alt="Arrow Up Icon" />
+                <p className="max_temp">26°</p>
               </div>
               <div className="tempBox">
-                <img width={"20px"} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJkPC6jNO4CP_14rB_W8VmlfpvF0uCuG2fU9z7geagHg&s" alt="" />
-                <p className="max_temp">26</p>
+                <img src={Arrow_Down_Icon} alt="Arrow Down Icon" />
+                <p className="min_temp">22°</p>
               </div>
             </div>
 
-            <p className="humidity row"><img width={"20px"} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJkPC6jNO4CP_14rB_W8VmlfpvF0uCuG2fU9z7geagHg&s" alt="" /> Humidity <p>28%</p></p>
-            <p className="wind row"><img width={"20px"} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJkPC6jNO4CP_14rB_W8VmlfpvF0uCuG2fU9z7geagHg&s" alt="" /> Wind <p>28kmh</p></p>
-            <p className="pressure row"><img width={"20px"} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJkPC6jNO4CP_14rB_W8VmlfpvF0uCuG2fU9z7geagHg&s" alt="" /> Pressure <p>28%</p></p>
+            <span className="row">
+              <img src={Humidity_Icon} alt="" />
+              <p className="humidity">Humidity</p>
+              <p>28%</p>
+            </span>
+            <span className="row">
+              <img src={Wind_Icon} alt="" />
+              <p className="wind"> Wind</p>
+              <p>28kmh</p>
+            </span>
+            <span className="row">
+              <img src={Pressure_Icon} alt="" />
+              <p className="pressure">Pressure</p>
+              <p>28%</p>
+            </span>
           </div>
         </div>
       </div>
